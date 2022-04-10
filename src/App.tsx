d14211d1bc23ce06cbe4069231dc8129e47fa3fc
Sreejan <sreejan147@gmail.com>
2022-04-10 20:36:57 +0530

    feat: add basic styles using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,108 @@
 import { useState } from "react";
 import { useTodoStore } from "./todoStore";
+import styled from "styled-components";
 import "./App.css";
+
+const Container = styled.div`
+  margin-top: 4rem;
+  width: 100vw;
+`;
+
+const TodoInputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const TodoInput = styled.input`
+  padding: 0.5rem;
+  font-size: 1rem;
+`;
+
+const AddTodoButton = styled.button`
+  padding: 0 0.5rem;
+  cursor: pointer;
+`;
+
+const TodosContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Todos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Todo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+`;
+
+const TodoLabel = styled.label<{ completed: boolean }>`
+  font-size: 1.2rem;
+  text-decoration: ${(props) => (props.completed ? "line-through" : "none")};
+`;
+
+const DeleteTodoBtn = styled.button`
+  padding: 0 0.5rem;
+`;
 
 function App() {
   const [todoInput, setTodoInput] = useState("");
   const { addTodo, removeTodo, toggleCompletedState, todos } = useTodoStore();
 
   return (
-    <div>
-      <h1>Todo App</h1>
+    <Container>
+      <h1 style={{ textAlign: "center" }}>Todo App</h1>
       <br />
       <br />
-      <input
-        type="text"
-        value={todoInput}
-        onChange={(e) => setTodoInput(e.target.value)}
-      />
-      &nbsp;&nbsp;&nbsp;
-      <button
-        onClick={() => {
-          if (todoInput.length > 0) {
-            addTodo(todoInput);
-          }
-        }}
-      >
-        Add Todo
-      </button>
+      <TodoInputContainer>
+        <TodoInput
+          type="text"
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
+        />
+        &nbsp;&nbsp;&nbsp;
+        <AddTodoButton
+          onClick={() => {
+            if (todoInput.length > 0) {
+              addTodo(todoInput);
+            }
+          }}
+        >
+          Add Todo
+        </AddTodoButton>
+      </TodoInputContainer>
       <br />
       <br />
-      <div className="todos">
-        {todos.map((todo) => (
-          <div className="todo" key={todo.id}>
-            <input
-              type="checkbox"
-              name=""
-              id=""
-              checked={todo.completed}
-              onChange={() => toggleCompletedState(todo.id)}
-              className={todo.completed ? "strikethrough" : ""}
-            />
-            <label htmlFor="" className={todo.completed ? "strikethrough" : ""}>
-              {todo.description}
-            </label>
-            &nbsp;&nbsp;&nbsp;
-            <button onClick={() => removeTodo(todo.id)}>Delete</button>
-          </div>
-        ))}
-      </div>
-    </div>
+      <TodosContainer>
+        <Todos>
+          <Todos>
+            {todos.map((todo) => (
+              <Todo key={todo.id}>
+                <input
+                  type="checkbox"
+                  name=""
+                  id={todo.id}
+                  checked={todo.completed}
+                  onChange={() => toggleCompletedState(todo.id)}
+                />
+                &nbsp;
+                <TodoLabel htmlFor={todo.id} completed={todo.completed}>
+                  {todo.description}
+                </TodoLabel>
+                &nbsp;&nbsp;&nbsp;
+                <DeleteTodoBtn onClick={() => removeTodo(todo.id)}>
+                  Delete
+                </DeleteTodoBtn>
+              </Todo>
+            ))}
+          </Todos>
+        </Todos>
+      </TodosContainer>
+    </Container>
   );
 }
 
